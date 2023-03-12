4056725520c184d766998068d97b2093664a4e43
carterbancroft <carter.bancroft@gmail.com>
2023-03-12 13:22:42 -0800

    Add material-ui and basic Autocomplete field

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Autocomplete
+        disablePortal
+        id="kit-autocomplete-search"
+        // options={}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Kit ID" />}
+      />
     </div>
   );
 }
