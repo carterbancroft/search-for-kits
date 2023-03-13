dee3bca2f9f50721edb2703c5ac44c9d802fffde
carterbancroft <carter.bancroft@gmail.com>
2023-03-13 11:50:21 -0800

    Add inline todos and comments

--- api/index.js
+++ api/index.js
@@ -5,14 +5,21 @@ const cors = require('cors');
 const app = express();
 const port = 8888;
 
+// TODO: This is just opening things up to all cross origin requests. It should
+// probably at least specify origins based on the current env.
 app.use(cors());
 
+// TODO: In a prod ready app, this data would be in a real database and there
+// would be lib code for managing database interaction. Just loading a JSON
+// file into memory on startup is obviously not a sustainable solution for a
+// real app.
 let dataJson = [];
 try {
   const data = fs.readFileSync('./KITS_SHIPPING_DATA.json', 'utf8');
   dataJson = JSON.parse(data);
 }
 catch (err) {
+  // TODO: Error handling would ideally report to a product like Rollbar/Sentry.
   console.error(err);
 }
 
@@ -23,7 +30,14 @@ app.get('/search', (req, res) => {
 
   const searchString = req.query.q;
 
+  // TODO: I'm trusting user input, which ain't great. If this were real and DB
+  // backed it should have validation so we don't fall prey to injection.
   const filteredData = dataJson.filter((d) => d.label_id.includes(searchString));
+
+  // TODO: This formats the response object to match what the Autocomplete
+  // requires on the FE. This might be the tail wagging the dog a bit, perhaps
+  // the FE should handle massaging the data so it fits into the Autocomplete's
+  // paradigm and the BE could just return the data as it comes from the DB.
   const responseObj = filteredData.map((d) => ({ label: d.label_id, id: d.id }));
 
   return res.json(responseObj);
@@ -35,11 +49,15 @@ app.get('/kit', (req, res) => {
     return res.json({});
   }
 
+  // TODO: Again, this would be validated in a real DB backed app.
   const kit = dataJson.find((d) => d.id === parsedId);
 
   return res.json(kit);
 });
 
+// TODO: In a prod ready app the server logic would be abstracted out some so
+// the endpoint definitions are mashed up with it. It was just quick and dirty
+// to put it all in the same file.
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
 
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
@@ -8,33 +8,56 @@ import KitDetail from './KitDetail';
 import './App.css';
 
 function App() {
+  // kitSearchData contains the current search results based on user input,
+  // formatted to work with the Autocomplete component.
   const [kitSearchData, setKitSearchData] = useState([]);
+
+  // kitData contains the detailed information for a selected kit, as returned
+  // from the API.
   const [kitData, setKitData] = useState();
+
+  // Tracks the ID for the currently selected kit. Used for making requests
+  // for specific kitData.
   const [kitId, setKitId] = useState();
+
+  // Tracks the current Autocomplete input. Used for making kit search requests.
   const [autocompleteValue, setAutocompleteValue] = useState();
 
+  // TODO: This is a bit of a code smell. It should be refactored such that the
+  // component itself doesn't need to know URL details and that the URL is env
+  // based.
   const apiUrl = 'http://localhost:8888'
 
   const fetchKitSearchData = async () => {
-    const endpoint = `${apiUrl}/search?q=${autocompleteValue}`
+    const endpoint = `${apiUrl}/search?q=${autocompleteValue}`;
     const response = await fetch(endpoint);
     const data = await response.json();
     setKitSearchData(data);
   }
 
   const fetchKitData = async () => {
-    const endpoint = `${apiUrl}/kit?id=${kitId}`
+    const endpoint = `${apiUrl}/kit?id=${kitId}`;
     const response = await fetch(endpoint);
     const data = await response.json();
     setKitData(data);
   }
 
+  // Whenever the autocomplete text value changes it should make a new search
+  // request to the API.
+  //
+  // TODO: This should be debounced.
   useEffect(() => {
     if (!autocompleteValue) return;
 
+    // TODO: In a prod ready application, error reporting would be more robust
+    // than just logging to the console. Depending on context it should either
+    // show something to the user, send an error to something like
+    // Rollbar/Sentry or both.
     fetchKitSearchData().catch(console.error);
   }, [autocompleteValue]);
 
+  // Whenever a Kit ID is selected, it should make a request to the backend to
+  // get kit details.
   useEffect(() => {
     if (!kitId) return;
 
