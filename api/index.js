0e5cfc0fdbaed3d8fe71f5ffbfce11b3a0406f12
carterbancroft <carter.bancroft@gmail.com>
2023-03-12 16:09:00 -0800

    Get search working appropriately from the backend and display fedex tracking

--- api/index.js
+++ api/index.js
@@ -1,21 +1,46 @@
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 8888;
 
+app.use(cors());
+
+let dataJson = [];
+try {
+  const data = fs.readFileSync('./KITS_SHIPPING_DATA.json', 'utf8');
+  dataJson = JSON.parse(data);
+}
+catch (err) {
+  console.error(err);
+}
+
 app.get('/search', (req, res) => {
-  let dataJson = [];
-  try {
-    const data = fs.readFileSync('./KITS_SHIPPING_DATA.json', 'utf8');
-    dataJson = JSON.parse(data);
+  if (!req.query?.q) {
+    return res.json([]);
   }
-  catch (err) {
-    console.error(err);
+
+  const searchString = req.query.q;
+
+  const filteredData = dataJson.filter((d) => d.label_id.includes(searchString));
+
+  const responseObj = filteredData.map((d) => ({ label: d.label_id, id: d.id }));
+
+  return res.json(responseObj);
+});
+
+app.get('/kit', (req, res) => {
+  const parsedId = parseInt(req.query?.id, 10);
+  if (!parsedId) {
+    return res.json({});
   }
-  res.json(dataJson);
+
+  const kit = dataJson.find((d) => d.id === parsedId);
+
+  return res.json(kit);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,56 @@
+import React, {useState, useEffect} from 'react'
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
 import './App.css';
 
 function App() {
+  const [kitSearchData, setKitSearchData] = useState([]);
+  const [kitData, setKitData] = useState({});
+  const [kitId, setKitId] = useState();
+  const [autocompleteValue, setAutocompleteValue] = useState();
+
+  const apiUrl = 'http://localhost:8888'
+
+  const fetchKitSearchData = async () => {
+    const endpoint = `${apiUrl}/search?q=${autocompleteValue}`
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    setKitSearchData(data);
+  }
+
+  const fetchKitData = async () => {
+    const endpoint = `${apiUrl}/kit?id=${kitId}`
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    setKitData(data);
+  }
+
+  useEffect(() => {
+    if (!autocompleteValue) return;
+
+    fetchKitSearchData().catch(console.error);
+  }, [autocompleteValue]);
+
+  useEffect(() => {
+    if (!kitId) return;
+
+    fetchKitData().catch(console.error);
+  }, [kitId])
+
   return (
     <div className="App">
       <Autocomplete
         disablePortal
         id="kit-autocomplete-search"
-        // options={}
+        options={kitSearchData}
+        filterOptions={(x) => x}
         sx={{ width: 300 }}
+        onChange={(_, value) => setKitId(value.id)}
+        onInputChange={(_, value) => setAutocompleteValue(value)}
         renderInput={(params) => <TextField {...params} label="Kit ID" />}
       />
+      <div>{kitData?.shipping_tracking_code}</div>
     </div>
   );
 }
